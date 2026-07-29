2254c095ac52a4534caa16d6d0371cf12dbe89c1
CodingProjectDev <yteasycode@gmail.com>
2026-07-29 16:15:09 -0500

    Change default message to 'Happy Birthday Ezekiel!'

--- countdown.js
+++ countdown.js
@@ -191,7 +191,7 @@ function cdInit() {
     setUrgent(false);
 
     var raw = messageInput.value || '';
-    var msg = raw.replace(/^\s+|\s+$/g, '') || '\uD83C\uDF86 Happy New Year! \uD83C\uDF86';
+    var msg = raw.replace(/^\s+|\s+$/g, '') || '🎆 Happy Birthday Ezekiel! 🎆';
     showZeroMessage(msg);
     playLaunchFanfare();
     triggerAutoLaunch();
